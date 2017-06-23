aa14c541660eab63fbb0f435d00c864e3aa8b22f
ADahlgren <adahlgren@ucdavis.edu>
2017-06-23 14:59:53 -0700

    Add sort, index, and flagstats

--- mapping_loop.sh
+++ mapping_loop.sh
@@ -14,8 +14,24 @@ echo $base2
 #map
 bwa mem -a -t 4 -M -R @RG"\t"ID:$base"\t"SM:$base"\t"PL:illumina"\t"LB:$base genome.fa ${base}.qc.fq ${base2}.qc.fq > ${base}_pe_aligned_reads.sam
 
+wait
+
 #convert to bam
+echo "converting to bam"
 samtools view -bS ${base}_pe_aligned_reads.sam > ${base}_pe.bam
 
+wait
+
 #sort
+echo "sorting"
 samtools sort ${base}_pe.bam > ${base}_pe_sorted.bam
+
+wait
+
+#index
+echo "indexing - almost there!"
+samtools index ${base}_pe_sorted.bam
+
+#run flagstats
+echo "get yo stats"
+samtools flagstat ${base}_pe_sorted.bam
