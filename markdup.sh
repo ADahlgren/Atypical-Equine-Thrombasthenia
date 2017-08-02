2ddc619bcefbe7938bd6cd8b77be0e97591b7550
Anna Dahlgren <adahlgren@ucdavis.edu>
2017-08-02 16:10:19 -0700

    Create markdup.sh

---
+++ markdup.sh
@@ -0,0 +1,13 @@
+#Load Picard Tools
+module load picard-tools/2.6.0
+
+#create bases
+for filename in *_sorted.bam
+do
+base=$(basename $filename .bam)
+echo $base
+
+#remove PCR duplicates 
+picard MarkDuplicates INPUT=${base}.bam OUTPUT=${base}_dedup.bam METRICS_FILE=${base}_metrics.txt
+
+done
