06b65ad0ca8c4611c9e12208357049fe0ae32ee6
Anna Dahlgren <adahlgren@ucdavis.edu>
2019-07-26 16:48:55 -0700

    Pipe so no sam files

--- 2_mapping_loop.sh
+++ 2_mapping_loop.sh
@@ -11,22 +11,9 @@ echo $base
 base2=${base/R1/R2}
 echo $base2
 
-#map
-bwa mem -a -t 4 -M -R @RG"\t"ID:$base"\t"SM:$base"\t"PL:illumina"\t"LB:$base genome.fa ${base}.qc.fq ${base2}.qc.fq > ${base}_pe_aligned_reads.sam
-
-wait
-
-#convert to bam
-echo "converting to bam"
-samtools view -bS ${base}_pe_aligned_reads.sam > ${base}_pe.bam
-
-wait
-
-#sort
-echo "sorting"
-samtools sort ${base}_pe.bam > ${base}_pe_sorted.bam
-
-wait
+#map, convert to bam, sort
+bwa mem -a -t 4 -M -R @RG"\t"ID:$base"\t"SM:$base"\t"PL:illumina"\t"LB:$base genome_3.0.fna ${base}.qc.fq.gz ${base2}.qc.fq.gz | \
+samtools view -bS | samtools sort > ${base}_pe_sorted.bam
 
 #index
 echo "indexing - almost there!"
