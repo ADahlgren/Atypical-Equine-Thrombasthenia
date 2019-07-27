a27d08f6906c60ba357eb721b5fb9d89c816c6e2
Anna Dahlgren <adahlgren@ucdavis.edu>
2019-07-26 17:01:06 -0700

    Update for new version

--- 7_delly.sh
+++ 7_delly.sh
@@ -1,6 +1,6 @@
-module load delly/0.5.9
+module load delly/0.7.8
 
 #-o = output file
 #-g = path to reference genome
 # list all bam files
-delly -o delly_all_var.vcf -g /share/finnolab/adahl/equCab2_UCSC_BWAIndex/genome.fa *_dedup.bam
+delly call -g genome_3.0.fna -o delly_var.bcf 114_S1_L001_R1_001_pe_sorted_dedup.bam 11_S7_L007_R1_001_pe_sorted_dedup.bam 129_S5_L005_R1_001_pe_sorted_dedup.bam 12_S8_L008_R1_001_pe_sorted_dedup.bam
